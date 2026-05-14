25c74c425d75e1c02246ad29a97e2eb76d4d224d
ofcorp <director@demrev.biz>
2026-05-15 00:35:21 +0300

    2-pages dz ispr1

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '../styles/base.css'
 import { RouterProvider } from 'react-router'
 import { router } from '../routes'
 
