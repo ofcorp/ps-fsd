26132322a4c3a8d25c38bba5434f1f90190fce14
ofcorp <director@demrev.biz>
2026-05-18 15:44:36 +0300

    3-shared dz

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './Button/Button';
+export { Checkbox } from './Checkbox/Checkbox';
+export { RadioButton } from './RadioButton/RadioButton';
+export { Input } from './Input/Input';
+export { Link } from './Link/Link';
